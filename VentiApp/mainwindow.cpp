84c511c4b1e6a03043b764c263f80394fe3222eb
KYS <juiceqwer@gmail.com>
2026-03-05 16:38:27 +0900

    ver1.0.0

--- VentiApp/mainwindow.cpp
+++ VentiApp/mainwindow.cpp
@@ -6,8 +6,83 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    this->cart_each = 0;
+    this->cart_price = 0;
+    this->select_lang = 0;      // 언어 초기설정
+    this->setup_db();      // DB 데이터 설정
+
+    // 시작 페이지 추가
+    this->page_start = new PageStart(this);
+    ui->ui_page_start->layout()->addWidget(page_start);
+
+    ui->stackedWidget->setCurrentIndex(PAGE_START);
 }
 
+void MainWindow::setup_db(){
+    this->db.setHostName("10.10.20.106");
+    this->db.setPort(3306);
+    this->db.setUserName("kiosk");
+    this->db.setPassword("1234");
+    this->db.setDatabaseName("The_Chaos");
+}
+
+/////////////////////// 핸들 함수 시작 //////////////////////////////////////
+void MainWindow::recv_data(){
+
+
+    switch(){
+
+        /////////////////// 카테고리 설정//////////////////////////////
+    case :
+
+        break;
+
+    case :
+
+        break;
+
+    case :
+
+        break;
+
+        /////////////////// 카테고리 설정 끝 //////////////////////////////
+
+        /////////////////// 메뉴 선택 설정//////////////////////////////
+    case :
+
+        break;
+
+    case :
+
+        break;
+
+    case :
+
+        break;
+
+        /////////////////// 메뉴 선택 끝 //////////////////////////////
+        /////////////////// 장바구니 설정//////////////////////////////
+    case :
+
+        break;
+
+    case :
+
+        break;
+
+    case :
+
+        break;
+
+        /////////////////// 장바구니 설정 끝 //////////////////////////////
+
+
+
+
+    }
+}
+////////////////////////////// 핸들 함수 끝 //////////////////////////////////////
 MainWindow::~MainWindow()
 {
     delete ui;
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
+#include <QStringList>
+#include <QDebug>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -12,12 +16,22 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+    enum addr_page {  };   // 페이지 구분용
 
 public:
     MainWindow(QWidget *parent = nullptr);
+
+    // 메서드 선언
+    void setup_db();
     ~MainWindow();
 
-private:
+
+
+private:   //기능 추가 함수들...
     Ui::MainWindow *ui;
+
+
+private slots:  // 슬롯, 데이터 수신
+    void recv_data(addr_page addr, QString title, QStringList data);
 };
 #endif // MAINWINDOW_H
